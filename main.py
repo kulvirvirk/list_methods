e8deb761cc362d1374f74d9243bfd567568df8c3
kulvirvirk <kvirkdev@gmail.com>
2020-02-15 00:17:17 +0000

    added the append() method

--- main.py
+++ main.py
@@ -8,3 +8,18 @@
 # 7. use clear () method to clear the list 
 
 
+
+# 1. create a list of fruits 
+# 2. using append(), add fruit to the list
+my_fruits_list = ['apple', 'banana', 'cherry']
+print(my_fruits_list)
+my_fruits_list.append('orange')
+print(my_fruits_list)
+print('----------------*****----------------')
+
+# 3. use insert(), to insert another fruit in the list
+# 4. use extend() method to add elements to the list
+# 5. use pop() method to pop one of the elements from list
+# 6. use remove() method to remove fruit from the list
+# 7. use clear () method to clear the list 
+
