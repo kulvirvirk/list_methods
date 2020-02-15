02b76efdb19331a7c0686db1d1893cf1e709683a
kulvirvirk <kvirkdev@gmail.com>
2020-02-15 00:14:52 +0000

    added requirement statement

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# 1. create a list of fruits 
+# 2. using append(), add fruit to the list
+# 3. use insert(), to insert another fruit in the list
+# 4. use extend() method to add elements to the list
+# 5. use pop() method to pop one of the elements from list
+# 6. use remove() method to remove fruit from the list
+# 7. use clear () method to clear the list 
+
+
